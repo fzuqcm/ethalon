de7191d0c690c97724091f485d43d97b36787db7
Richard Hájek <richard.m.hajek@gmail.com>
2020-08-12 16:26:16 +0200

    Reading now works

--- src/main.js
+++ src/main.js
@@ -1,24 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import * as icons from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './assets/tailwind.css'
 
-// library.add(...s(icons))
-
-Vue.component('fa-icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
-
-//const serialport = require('serialport')
-//
-//
-//serialport.list().then((ports) => {
-//  console.log('ports', ports)
-//}).catch(err => {
-//})
 
 new Vue({
   store,
