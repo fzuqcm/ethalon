acca04259ef3326c5463b22bb99c510018a68fc6
lukeeno <lukeeno@pm.me>
2021-06-18 19:17:24 +0200

    update firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,59 +1,7 @@
-/***********************************************************************************************
-
-   LICENSE
-   Copyright (C) 2018 openQCM
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see http://www.gnu.org/licenses/gpl-3.0.txt
-  --------------------------------------------------------------------------------
-   OPENQCM Q-1 - Quartz Crystal Microbalance with dissipation monitoring
-   openQCM is the unique opensource quartz crystal microbalance http://openqcm.com/
-   ELECTRONICS
-     - board and firmware designed for teensy 3.6 development board
-     - DDS/DAC Synthesizer AD9851
-     - phase comparator AD8302
-     - I2C digital potentiometer AD5251+
-     - MCP9808 temperature sensor
-
-   version  ver 2.1
-   date     December 2018
-   author   openQCM team
-   --------------------------------------------------------------------------------
-
-   CHANGES
-   - changed pot value for compatibility with openQCM Q-1 shield @5VDC
-   - changed ADC delay microseconds > 100 us
-   - changed ADC avaeraging sample  > 32 sample
-   - changed the type of phase and mag to double
-   - changed included library for MCP9808 temperature sensor in skecth directory v1.0
-
-   TODO
-   - I2C replace standard i2c library with Teensy i2c custom library i2c_t3.h
-   Brian (nox771) New I2C library for Teensy3
-   https://github.com/nox771/i2c_t3
-   - ADC libraries https://github.com/pedvide/ADC
-
-   CREDIT:
-   - based on the work made by Brett Killion on Hackaday
-   https://hackaday.io/project/10021-arduino-network-analyzer
-   - Teensy 3.6 is  developed by Paul Stoffregen working at PJRC
-   https://www.pjrc.com/store/teensy36.html
-   - MCP9808 I2c temperature sensor dirver is developed by Adafruit
-   Written by Kevin Townsend/Limor Fried for Adafruit Industries.
-
- ***********************************************************************************************/
 
 /************************** LIBRARIES **************************/
-#include<BasicLinearAlgebra.h>
-#include<Wire.h>
+#include <BasicLinearAlgebra.h>
+#include <Wire.h>
 #include "src/Adafruit_MCP9808.h"
 #include <ADC.h>
 
@@ -66,7 +14,6 @@
 #define REFCLK 125000000
 
 /*************************** VARIABLE DECLARATION ***************************/
-
 // current input frequency
 long freq = 0;
 // DDS Synthesizer AD9851 pin function
@@ -88,7 +35,7 @@ double val = 0;
 // Create the MCP9808 temperature sensor object
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 // init temperature variable
-float temperature = 0;
+//float temperature = 0;
 
 // LED pin
 int ledPin1 = 24;
@@ -97,7 +44,7 @@ int ledPin2 = 25;
 // ADC init variabl
 boolean WAIT = true;
 // ADC waiting delay microseconds
-int WAIT_DELAY_US = 100;
+int WAIT_DELAY_US = 300;
 // ADC averaging
 boolean AVERAGING = true;
 // inint number of averaging
@@ -105,31 +52,18 @@ int AVERAGE_SAMPLE = 32;
 // teensy ADC averaging init
 int ADC_RESOLUTION = 13;
 
-// init sweep param
-long freq_start;
-long freq_stop;
-long freq_step;
-
 // init output ad8302 measurement (cast to double)
 double measure_phase = 0;
 double measure_mag = 0;
 
-const int STEP_NUM = 32;
-const int DEFAULT_CALIB_FREQ = 10000000;
-const int DEFAULT_RANGE = 65536;
-const int stepp = 16;
-const int r = 2048;
-const int cnt = r / stepp + 1;
-
-long calib_freq = DEFAULT_CALIB_FREQ;
-/*************************** FUNCTION ***************************/
+/*************************** FUNCTIONS ***************************/
 
 /* AD9851 set frequency fucntion */
 void SetFreq(long frequency)
 {
   // set to 125 MHz internal clock
   temp_FTW = (frequency * pow(2, 32)) / REFCLK;
-  FTW = long (temp_FTW);
+  FTW = long(temp_FTW);
 
   long pointer = 1;
   int pointer2 = 0b10000000;
@@ -138,8 +72,10 @@ void SetFreq(long frequency)
   /* 32 bit dds tuning word frequency instructions */
   for (int i = 0; i < 32; i++)
   {
-    if ((FTW & pointer) > 0) digitalWrite(DATA, HIGH);
-    else digitalWrite(DATA, LOW);
+    if ((FTW & pointer) > 0)
+      digitalWrite(DATA, HIGH);
+    else
+      digitalWrite(DATA, LOW);
     digitalWrite(WCLK, HIGH);
     digitalWrite(WCLK, LOW);
     pointer = pointer << 1;
@@ -160,82 +96,6 @@ void SetFreq(long frequency)
   digitalWrite(FQ_UD, LOW);
 
   //FTW = 0;
-}
-
-// 65536
-long gradient(long left_freq, long right_freq) {
-  int step_size = (right_freq - left_freq) / STEP_NUM;
-  if (step_size <= 1) {
-    return left_freq + (STEP_NUM / 2) * step_size;
-  }
-
-  int a;
-  int max_a = 0;
-  int max_f = left_freq;
-
-  for (long f = left_freq; f <= right_freq; f += step_size) {
-    SetFreq(f);
-    a = readQ();
-//    Serial.println(a);
-
-    if (a > max_a) {
-//      Serial.println(a);
-      max_a = a;
-      max_f = f;
-    }
-  }
-
-  return gradient(max_f - 4 * step_size, max_f + 2 * step_size);
-}
-
-int readAmpl() {
-  return analogRead(AD8302_MAG);
-}
-
-int readPhas() {
-  return analogRead(AD8302_PHASE);
-}
-
-int readQ() {
-  return readAmpl();
-}
-
-void resonanceFrequencyOutput(long range)
-{
-    long rf = gradient(calib_freq - range, calib_freq + range);
-
-    BLA::Matrix<cnt,3> A;
-    BLA::Matrix<cnt> b;
-
-    long f = rf - (r / 2);
-    int steps = 8;
-    double cumsum = 0;
-
-    for (int i = 0; i < cnt; i++, f+= stepp) {
-      SetFreq(f);
-      cumsum = 0;
-
-      for (int j = 0; j < steps; j++) {
-        cumsum += readQ();
-
-        b(i) = cumsum / (double) steps;
-        A(i, 0) = i*i;
-        A(i, 1) = i;
-        A(i, 2) = 1;
-      }
-    }
-
-    BLA::Matrix<3> coeffs = ((~A) * A).Inverse() * (~A) * b;
-    double result = -coeffs(1)/(2*coeffs(0)) * 16 + (double) rf;
-
-    calib_freq = (int) result;
-
-    Serial.println(result);
-
-    tempsensor.shutdown_wake(0);  // Don't remove this line! required before reading temp
-    temperature = tempsensor.readTempC();
-
-    Serial.println(temperature);
 }
 
 /*************************** SETUP ***************************/
@@ -283,6 +143,195 @@ void setup()
   pinMode(ledPin2, OUTPUT);
   digitalWrite(ledPin1, HIGH);
   digitalWrite(ledPin2, HIGH);
+
+  while (!Serial)
+  {
+
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+int legacyRead(String msg)
+{
+  int d0 = msg.indexOf(';');
+  int d1 = msg.indexOf(';', d0 + 1);
+
+  if (d0 == -1 || d1 == -1 || d0 == d1)
+  {
+    return 1;
+  }
+
+  long f0 = msg.substring(0, d0).toInt();
+  long f1 = msg.substring(d0 + 1, d1).toInt();
+  long f2 = msg.substring(d1 + 1).toInt();
+
+  if (!f0 || !f1 || !f2)
+  {
+    return 2;
+  }
+
+  // 9990000;10010000;40
+  double measure_phase = 0;
+  double measure_mag = 0;
+
+  for (long f = f0; f <= f1; f += f2)
+  {
+    SetFreq(f);
+    if (WAIT)
+      delayMicroseconds(WAIT_DELAY_US);
+
+    int app_phase = 0;
+    int app_mag = 0;
+
+    for (int i = 0; i < AVERAGE_SAMPLE; i++)
+    {
+      app_phase += analogRead(AD8302_PHASE);
+      app_mag += analogRead(AD8302_MAG);
+    }
+
+    measure_phase = 1.0 * app_phase / AVERAGE_SAMPLE;
+    measure_mag = 1.0 * app_mag / AVERAGE_SAMPLE;
+
+    Serial.print(measure_mag);
+    Serial.print(";");
+    Serial.print(measure_phase);
+    Serial.println();
+  }
+
+  tempsensor.shutdown_wake(0);
+  float temperature = tempsensor.readTempC();
+
+  Serial.print(temperature);
+  Serial.print(";");
+  Serial.println("s");
+
+  return 0;
+}
+
+const int DIRTY_NUM = 64;
+const int DIRTY_RANGE = 16384;
+
+long gradient1(long left_freq, long right_freq)
+{
+  int step_size = (right_freq - left_freq) / DIRTY_NUM;
+  if (step_size <= 1)
+  {
+    return left_freq + (DIRTY_NUM / 2) * step_size;
+  }
+
+  int a;
+  int max_a = 0;
+  int max_f = 0;
+
+  for (long f = left_freq; f <= right_freq; f += step_size)
+  {
+    SetFreq(f);
+    if (WAIT)
+      delayMicroseconds(WAIT_DELAY_US);
+    a = analogRead(AD8302_MAG);
+
+    if (a > max_a)
+    {
+      max_a = a;
+      max_f = f;
+    }
+  }
+
+  return gradient1(max_f - 2 * step_size, max_f + 2 * step_size);
+}
+
+const int DEFAULT_CALIB_FREQ = 10000000;
+const int DEFAULT_RANGE = 65536;
+const int SWEEP_STEP = 32;
+const int SWEEP_RANGE = 3072;
+const int SWEEP_COUNT = SWEEP_RANGE / SWEEP_STEP + 1;
+const int SWEEP_REPEAT = 16;
+
+long calib_freq = DEFAULT_CALIB_FREQ;
+
+double sweepFrequency(long rf)
+{
+  String str = String();
+  BLA::Matrix<SWEEP_COUNT, 3> A;
+  BLA::Matrix<SWEEP_COUNT> b;
+
+  long f = rf - (SWEEP_RANGE / 2);
+  double cumsum = 0;
+
+  SetFreq(f);
+  delayMicroseconds(WAIT_DELAY_US * 3);
+  analogRead(AD8302_MAG);
+
+  for (int i = 0; i < SWEEP_COUNT; i++, f += SWEEP_STEP)
+  {
+    SetFreq(f);
+    if (WAIT)
+      delayMicroseconds(WAIT_DELAY_US);
+    cumsum = 0;
+
+    for (int j = 0; j < SWEEP_REPEAT; j++)
+    {
+      cumsum += analogRead(AD8302_MAG);
+    }
+
+    b(i) = cumsum / (double)SWEEP_REPEAT;
+    A(i, 0) = i * i;
+    A(i, 1) = i;
+    A(i, 2) = 1;
+
+    if (b(i) < 4000)
+    {
+      return -1;
+    }
+
+    str.concat(b(i)).concat(';');
+  }
+
+  //  Serial.println(str);
+
+  BLA::Matrix<3> coeffs = ((~A) * A).Inverse() * (~A) * b;
+  double result = -coeffs(1) / (2 * coeffs(0)) * (double)SWEEP_STEP + (double)rf;
+
+  //  Serial.println(result);
+
+  calib_freq = (long)result;
+
+  return result;
+}
+
+int modernRead(String msg)
+{
+  long param = msg.substring(2).toInt();
+  char cmd = msg.charAt(0);
+  long f;
+  double rf;
+  float t;
+
+  switch (cmd)
+  {
+  case 'c':
+    f = gradient1(DEFAULT_CALIB_FREQ - DIRTY_RANGE, DEFAULT_CALIB_FREQ + DIRTY_RANGE);
+    rf = sweepFrequency(f);
+    Serial.println(rf);
+    tempsensor.shutdown_wake(0);
+    t = tempsensor.readTempC();
+    Serial.println(t);
+    break;
+  case 'm':
+    f = gradient1(calib_freq - DIRTY_RANGE, calib_freq + DIRTY_RANGE);
+    rf = sweepFrequency(f);
+    Serial.println(rf);
+    tempsensor.shutdown_wake(0);
+    t = tempsensor.readTempC();
+    Serial.println(t);
+    break;
+  case 'p':
+    break;
+  default:
+    return 1;
+  }
+
+  return 0;
 }
 
 int message = 0;
@@ -295,120 +344,24 @@ int byteAtPort = 0;
 /*************************** LOOP ***************************/
 void loop()
 {
-  if ( (byteAtPort = Serial.available()) > 0) {
-    // read message at serial port
-    String message_str = Serial.readStringUntil('\n');
-    char buf[byteAtPort];
-    message_str.toCharArray(buf, sizeof(buf));
-    char *p = buf;
-    char *str;
-    int nn = 0;
-
-    bool isCalibration = strchr(buf, 'c') != NULL;
-    if (isCalibration)
-        calib_freq = DEFAULT_CALIB_FREQ;
-
-    char* receivedCalibFreq = strchr(buf, 'm');
-    bool isMeasurement = false;
-    if (receivedCalibFreq != NULL) {
-        if (strlen(receivedCalibFreq) > 1)
-            calib_freq = atol(++receivedCalibFreq);
-
-        isMeasurement = true;
-    }
-
-    if (isCalibration || isMeasurement) {
-        resonanceFrequencyOutput(DEFAULT_RANGE);
-        return;
-    }
-
-    bool isBinary = strchr(buf, '|') != NULL;
-    // decode message
-    while (isBinary ? (str = strtok_r(p, "|", &p)) != NULL : (str = strtok_r(p, ";", &p)) != NULL) { // delimiter is the semicolon
-      // frequency start
-      if (nn == 0) {
-        freq_start = atol(str);
-        // Serial.print("FREQ START = ");
-        // Serial.println(freq_start);
-        nn = 1;
-      }
-      // frequency stop
-      else if (nn == 1) {
-        freq_stop = atol(str);
-        nn = 2;
-      }
-      // frequency step
-      else if (nn == 2) {
-        freq_step = atol(str);
-        nn = 0;
-        message = 1;
-      }
-    }
-
-    if (message == 0) {
-      // dummy do noything
-    }
-    if (message == 1) {
-      // start sweep
-      long count = 0;
-      pre_time = millis();
-      int stepCount = (freq_stop - freq_start) / freq_step + 1;
-      int magBuffer[stepCount];
-      int phaseBuffer[stepCount];
-      int *pMagBuffer = magBuffer;
-      int *pPhaseBuffer = phaseBuffer;
-      // start sweep cycle measurement
-      for (count = freq_start; count <= freq_stop; count = count + freq_step)
-      {
-        // set AD9851 DDS current frequency
-        SetFreq(count);
-        // do the magic ! waiting for the ADC measure
-        if (WAIT) delayMicroseconds(WAIT_DELAY_US);
-
-        // measure gain phase
-        int  app_phase = 0;
-        int app_mag = 0;
-
-        // ADC measure and averaging
-        if (AVERAGING == true) {
-          for (int i = 0; i < AVERAGE_SAMPLE; i++) {
-            // ADC measure phase
-            app_phase += analogRead(AD8302_PHASE);
-            // ADC measure gain
-            app_mag += analogRead(AD8302_MAG);
-          }
-          // averaging (cast to double)
-          measure_phase = 1.0 * app_phase / AVERAGE_SAMPLE;
-          measure_mag = 1.0 * app_mag / AVERAGE_SAMPLE;
-
-          if (isBinary) {
-            *pMagBuffer = app_mag;
-            *pPhaseBuffer = app_phase;
-            pMagBuffer++;
-            pPhaseBuffer++;
-          } else {
-            // serial write data
-            Serial.print(measure_mag);
-            Serial.print(";");
-            Serial.print(measure_phase);
-            Serial.println();
-          }
-        }
-      }
-      // measure temperature
-      tempsensor.shutdown_wake(0);  // Don't remove this line! required before reading temp
-      temperature = tempsensor.readTempC();
-      if (isBinary) {
-        Serial.write((uint8_t*)magBuffer, sizeof(magBuffer));
-        Serial.write((uint8_t*)phaseBuffer, sizeof(phaseBuffer));
-        Serial.write((uint8_t*)&temperature, sizeof(temperature));
-      } else {
-        // serial write temperature data at the end of the sweep
-        Serial.print(temperature);
-        Serial.print(";");
-        // print termination char EOM
-        Serial.println("s");
-      }
-    }
+  if (!(Serial.available() > 0))
+  {
+    return;
   }
+
+  String msg = Serial.readStringUntil('\n');
+  bool useModern = msg.indexOf(';') == -1;
+
+  int retVal;
+  if (useModern)
+  {
+    retVal = modernRead(msg);
+  }
+  else
+  {
+    retVal = legacyRead(msg);
+  }
+
+  Serial.println("Modern: " + String(useModern));
+  Serial.println("RetVal: " + String(retVal));
 }
